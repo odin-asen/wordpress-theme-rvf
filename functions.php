8214d328fb1933de51e5d8634074af144b49fc01
Timm Herrmann <timm.herrmann@freenet.de>
2018-09-02 03:06:29 +0200

    Header wird HTML-konform

--- functions.php
+++ functions.php
@@ -125,20 +125,19 @@ add_action( 'widgets_init', 'rvf_widgets_init' );
 /**
  * Enqueue scripts and styles.
  */
- /*
 function rvf_scripts() {
-	wp_enqueue_style( 'rvf-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'rvf-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'rvf-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'rvf_scripts' );
-*/
+
 /**
  * Custom functions that act independently of the theme templates
  */
